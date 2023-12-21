d3b12d96a12409419e8595dcff98a8097473e06e
Eli Manzo <96manzo.eli@gmail.com>
2023-12-21 14:27:10 -0800

    Adding post template file

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function eliThemeSupport() {
   // Adds Dynamic title
   add_theme_support('title-tag');
   add_theme_support('custom-logo');
+  add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'eliThemeSupport');
